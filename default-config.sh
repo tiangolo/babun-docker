b8d7fe6715cab2d68bb4bceab0c4fb0c515ad588
sebastián Ramírez <tiangolo@gmail.com>
2016-04-18 16:47:30 -0500

    Using cygwin VBoxManage path

--- default-config.sh
+++ default-config.sh
@@ -1,3 +1,4 @@
+#! /usr/bin/env bash
 # Set variables
 
 # Should babun-docker setup volumes?
@@ -5,3 +6,7 @@ babun_docker_setup_volumes=1
 
 # docker-machine name
 babun_docker_machine_name="default"
+
+# VirtualBox path
+# C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage
+babun_docker_virtualbox_bin="/cygdrive/c/Program Files/Oracle/VirtualBox/VBoxManage"
