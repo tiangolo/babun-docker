4f9a4d6731dfd87f5a0398c754d48d9833eddc7a
sebastián Ramírez <tiangolo@gmail.com>
2016-04-20 14:43:03 -0500

    Update winpty to latest version including updating existing winpty

--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 
 # winpty base URL
-babun_docker_WINPTY_BASE_URL="https://github.com/downloads/rprichard/winpty/"
+babun_docker_winpty_base_url="https://github.com/rprichard/winpty/releases/download/0.2.2/"
+
+
 # Specific file name, separated to allow unzipping it later
-babun_docker_WINPTY_FILENAME="winpty-0.1.1-cygwin.zip"
+babun_docker_winpty_only_name="winpty-0.2.2-cygwin-2.4.1-ia32"
+
+# Specific file extension
+babun_docker_winpty_ext=".tar.gz"
+
+# Complete filename
+babun_docker_winpty_filename="$babun_docker_winpty_only_name$babun_docker_winpty_ext"
 
 # babun-docker repo
 babun_docker_repo='https://github.com/tiangolo/babun-docker.git'
@@ -14,24 +22,33 @@ babun_docker_feedback='-- babun-docker:'
 babun_docker_OLD_PWD=$(pwd)
 # Set up winpty
 # Where to download winpty from
-babun_docker_WINPTY_URL="$babun_docker_WINPTY_BASE_URL$babun_docker_WINPTY_FILENAME"
+babun_docker_winpty_url="$babun_docker_winpty_base_url$babun_docker_winpty_filename"
 # Directory in where to put Winpty
-babun_docker_WINPTY_DIR="$HOME/.winpty"
+babun_docker_winpty_dir="$HOME/.winpty"
 # Create a local .winpty directory under your home
-if [[ ! -d $babun_docker_WINPTY_DIR ]] ; then
-   mkdir -p $babun_docker_WINPTY_DIR ;
+if [[ ! -d $babun_docker_winpty_dir ]] ; then
+   mkdir -p $babun_docker_winpty_dir ;
 fi
 # Enter the .winpty directory to download winpty
-cd $babun_docker_WINPTY_DIR
-if [[ ! -f $babun_docker_WINPTY_FILENAME ]] ; then
+cd $babun_docker_winpty_dir
+if [[ ! -f $babun_docker_winpty_filename ]] ; then
+   # Remove old files
+   cd ..
+   rm -rf $babun_docker_winpty_dir
+   mkdir -p $babun_docker_winpty_dir
+   cd $babun_docker_winpty_dir
    # Download winpty
-   wget $babun_docker_WINPTY_URL
-   # Unzip the downloaded file
-   unzip $babun_docker_WINPTY_FILENAME
+   wget $babun_docker_winpty_url
+   # Untar the downloaded file
+   tar xf $babun_docker_winpty_filename
+   # Move the tar contents to the current directory
+   mv $babun_docker_winpty_only_name/bin/* ./
+   # Remove untar-ed directory
+   rm -rf $babun_docker_winpty_only_name
    # Make console executable
-   chmod 777 $babun_docker_WINPTY_DIR/*
+   chmod 777 $babun_docker_winpty_dir/*
 fi
-export PATH=$PATH:$babun_docker_WINPTY_DIR
+export PATH=$PATH:$babun_docker_winpty_dir
 
 
 # Set up babun-docker
